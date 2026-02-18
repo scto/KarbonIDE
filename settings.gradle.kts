5537c1f55b49b9bab160a6eb008c26051a193d49
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-02-18 20:41:08 +0100

    Refactor repository management in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,27 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-        maven { url = uri("https://jitpack.io")}
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+    maven("https://cache-redirector.jetbrains.com/kotlin.bintray.com/kotlin-plugin")
+  }
 }
+
+//plugins {
+//  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+//}
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io")}
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+    maven { url = uri("https://jitpack.io") }
+  }
 }
 
 rootProject.name = "KarbonIDE"
